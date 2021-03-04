2dcb00b2d04f46d8608cf99eae0c914ecf014744
vinit <vks9091@gmail.com>
2021-03-04 17:13:14 +0530

    project commited

---
+++ Test/src/com/app/test/HackerRank.java
@@ -0,0 +1,20 @@
+package com.app.test;
+import java.util.*;
+import java.io.*;
+
+public class HackerRank {
+	
+	public static void main(String[] args) {
+		
+
+		
+		        Scanner in = new Scanner(System.in);
+		        int t=in.nextInt();
+		        for(int i=0;i<t;i++){
+		            int a = in.nextInt();
+		            int b = in.nextInt();
+		            int n = in.nextInt();
+		           
+		    }
+	}
+}
